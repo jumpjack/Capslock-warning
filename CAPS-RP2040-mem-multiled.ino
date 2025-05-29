45c65ddd866a76d910ad48228949507828478f20
jumpjack <jumpjack@libero.it>
2025-05-29 15:56:11 +0200

    Update CAPS-RP2040-mem-multiled.ino

--- CAPS-RP2040-mem-multiled.ino
+++ CAPS-RP2040-mem-multiled.ino
@@ -2,6 +2,8 @@
 // External RGB LED support.
 // Added support for persistant storage of color.
 // Separated LEDs version: capslock = red, scroll-lock = green, numlock = blue; keys combination lead to colors combination
+// Imple,ented calibrarition level for RGB leds with unbalanced channels.
+
 
 #include <Keyboard.h>
 #include <Adafruit_NeoPixel.h>
@@ -18,34 +20,40 @@
 #define HIGH_GREEN 255
 #define HIGH_BLUE 255
 
+const uint8_t R_CALIB = 145;   
+const uint8_t G_CALIB = 255;    
+const uint8_t B_CALIB = 255;   
+
 #define COMMON_VALUE HIGH
 
 // Calcola i valori ON/OFF in base al tipo di LED (common anode o cathode)
-int RED_ON = (COMMON_VALUE == LOW) ? HIGH_RED : LOW;
-int RED_OFF = (COMMON_VALUE == LOW) ? LOW : HIGH_RED;
-int GREEN_ON = (COMMON_VALUE == LOW) ? HIGH_GREEN : LOW;
-int GREEN_OFF = (COMMON_VALUE == LOW) ? LOW : HIGH_GREEN;
-int BLUE_ON = (COMMON_VALUE == LOW) ? HIGH_BLUE : LOW;
-int BLUE_OFF = (COMMON_VALUE == LOW) ? LOW : HIGH_BLUE;
+int RED_ON = (COMMON_VALUE == LOW) ? R_CALIB : 255-R_CALIB;
+int RED_OFF = (COMMON_VALUE == LOW) ? 0 : 255;
 
-Adafruit_NeoPixel pixel(NUM_PIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+int GREEN_ON = (COMMON_VALUE == LOW) ? G_CALIB : 255-G_CALIB;
+int GREEN_OFF = (COMMON_VALUE == LOW) ? 0 : 255;
 
-// Colori base per ciascun tasto
-#define CAPS_RED 255    // Rosso puro per CapsLock
+int BLUE_ON = (COMMON_VALUE == LOW) ? B_CALIB : 255-B_CALIB;
+int BLUE_OFF = (COMMON_VALUE == LOW) ? 0 : 255;
+
+Adafruit_NeoPixel pixel(NUM_PIXELS, PIXEL_PIN, NEO_RGB + NEO_KHZ800);
+
+// Colori base bilanciati per ciascun tasto
+#define CAPS_RED 255  
 #define CAPS_GREEN 0
 #define CAPS_BLUE 0
 
-#define NUM_RED 0       // Blu puro per NumLock (invertito)
-#define NUM_GREEN 0
-#define NUM_BLUE 255
-
-#define SCROLL_RED 0    // Verde puro per ScrollLock
+#define SCROLL_RED 0    
 #define SCROLL_GREEN 255
 #define SCROLL_BLUE 0
 
+#define NUM_RED 0      
+#define NUM_GREEN 0
+#define NUM_BLUE 255
+
 void ledCallback(bool numLock, bool capsLock, bool scrollLock, bool compose, bool kana, void* data) {
   (void)compose; (void)kana; (void)data;
-  
+
   uint8_t r = 0, g = 0, b = 0;
   uint8_t rLed = RED_OFF, gLed = GREEN_OFF, bLed = BLUE_OFF;
 
@@ -75,14 +83,14 @@ void ledCallback(bool numLock, bool capsLock, bool scrollLock, bool compose, boo
     constrain(g, 0, 255),
     constrain(b, 0, 255)
   ));
-  
+
   // Gestione dei LED separati (se presenti)
   analogWrite(PIN_R, r > 0 ? rLed : RED_OFF);
   analogWrite(PIN_G, g > 0 ? gLed : GREEN_OFF);
   analogWrite(PIN_B, b > 0 ? bLed : BLUE_OFF);
-  
+
   pixel.show();
-  
+
   // Debug su seriale
   Serial.print("LED Status - ");
   Serial.print("NumLock: ");
@@ -101,6 +109,9 @@ void ledCallback(bool numLock, bool capsLock, bool scrollLock, bool compose, boo
   Serial.println(b);
 }
 
+
+
+
 void setup() {
   Serial.begin(115200);
   pixel.begin();
@@ -110,9 +121,11 @@ void setup() {
   // Configura il callback per i LED
   Keyboard._ledCB = ledCallback;
   Keyboard._ledCBdata = nullptr;
-  
+
   Keyboard.begin();
-  
+
+  delay(3000);
+
   Serial.println("Sistema pronto. Combinazione colori:");
   Serial.println("Caps Lock: Rosso");
   Serial.println("Num Lock: Blu (comportamento INVERTITO)");
@@ -128,7 +141,8 @@ void setup() {
   pinMode(PIN_B, OUTPUT);
   pinMode(PIN_COMMON, OUTPUT);
 
-  digitalWrite(PIN_COMMON, COMMON_VALUE); 
+  digitalWrite(PIN_COMMON, COMMON_VALUE);
+
 }
 
 void loop() {
